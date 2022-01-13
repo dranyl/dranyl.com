9c90852ef718b1daad16b51e1937149b6b8f0632
dranyl <dranyl@outlook.com>
2022-01-14 12:42:52 +1300

    changed testimonials page

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,5 +1,24 @@
 import './testimonials.scss'
 
 export default function Testimonials() {
-  return <div className='testimonials' id='testimonials'></div>
+  return (
+    <div className='testimonials' id='testimonials'>
+      <h1>Testimonials</h1>
+      <div className='container'>
+        <div className='card'>
+          <div className='top'>
+            <img src='assets/featured_webapp.png' alt='' />
+          </div>
+          <div className='center'>
+            This is my personal website using ReactJS framework and SaSS for css
+            styling.
+          </div>
+          <div className='bottom'>
+            <h3>https://dranyl.com</h3>
+            <h4>This website</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
