c9ee8ee54ae5181aa31b5d089b8eebb5531ebeaf
dranyl <dranyl@outlook.com>
2022-02-09 11:03:48 +1300

    added blog entry

--- src/data.js
+++ src/data.js
@@ -2,12 +2,28 @@ export const data_all = [
   {
     id: 1,
     title: 'Create Serverless Function using Netlify',
-    img: './assets/blog1.png',
+    img:
+      'https://dranyl.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Funsplash%2Fs0XabTAKvak%2Fupload%2Fv1643325022029%2F91POis16D.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75',
     url: 'https://dranyl.hashnode.dev/create-serverless-function-using-netlify',
+  },
+  {
+    id: 2,
+    title: 'Eclipse keyboard shortcut compilation',
+    img:
+      'https://dranyl.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Funsplash%2F7YiZKj9A3DM%2Fupload%2Fv1644215060857%2FNGYxaum7s.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75',
+    url: 'https://dranyl.hashnode.dev/eclipse-keyboard-shortcut-compilation',
   },
 ]
 
-export const data_java = []
+export const data_java = [
+  {
+    id: 1,
+    title: 'Eclipse keyboard shortcut compilation',
+    img:
+      'https://dranyl.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Funsplash%2F7YiZKj9A3DM%2Fupload%2Fv1644215060857%2FNGYxaum7s.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75',
+    url: 'https://dranyl.hashnode.dev/eclipse-keyboard-shortcut-compilation',
+  },
+]
 
 export const data_react = []
 
@@ -17,7 +33,8 @@ export const data_others = [
   {
     id: 1,
     title: 'Create Serverless Function using Netlify',
-    img: './assets/blog1.png',
+    img:
+      'https://dranyl.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Funsplash%2Fs0XabTAKvak%2Fupload%2Fv1643325022029%2F91POis16D.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75',
     url: 'https://dranyl.hashnode.dev/create-serverless-function-using-netlify',
   },
 ]
